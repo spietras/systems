6aa4836186fc59c5ef97585bac4ac1f365cd9a28
Sebastian Pietras <me@spietras.dev>
2023-04-14 23:10:04 +0200

    Added update workflow

---
+++ scripts/update.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+### HELPER FUNCTIONS ###
+
+print_usage() {
+    # Print script usage
+
+    cat <<EOF
+Usage: $0
+Update flake dependencies.
+EOF
+}
+
+### PARSE ARGUMENTS ###
+
+unparsed=''
+
+while [ -n "${1:-}" ]; do
+    case "$1" in
+    -h | --help)
+        print_usage >&2
+        exit
+        ;;
+    --)
+        shift
+        unparsed="$unparsed $*"
+        break
+        ;;
+    *) unparsed="$unparsed $1" ;;
+    esac
+    shift
+done
+
+# shellcheck disable=SC2086
+set -- $unparsed
+
+### MAIN ###
+
+nix --extra-experimental-features 'nix-command flakes' flake update "$@"
