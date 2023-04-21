a0d75070d84cf9af4d47e8bd9b44f0eefb32f209
Sebastian Pietras <me@spietras.dev>
2023-04-21 14:08:35 +0200

    Fixed picking up SSH keys by sops

--- hosts/xenon/modules/secrets/default.nix
+++ hosts/xenon/modules/secrets/default.nix
@@ -12,9 +12,17 @@
     age = {
       # age private key should be stored at this path on the host
       keyFile = "/${config.constants.storage.partitions.main.datasets.hardstate.label}/sops/age/keys.txt";
+
+      # This is needed so that ssh keys are not unnecessarily picked up
+      sshKeyPaths = [];
     };
 
     defaultSopsFile = ../../secrets.yaml;
+
+    gnupg = {
+      # This is needed so that ssh keys are not unnecessarily picked up
+      sshKeyPaths = [];
+    };
 
     # You need to explicitly list here all secrets you want to use
     secrets = {
