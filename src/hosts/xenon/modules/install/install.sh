0a37c20544e0426f46f6486e72121e29a9db8c41
Sebastian Pietras <me@spietras.dev>
2024-05-03 01:33:04 +0200

    Cleaned up storage configuration (#43)

--- src/hosts/xenon/modules/install/install.sh
+++ src/hosts/xenon/modules/install/install.sh
@@ -4,6 +4,7 @@
 
 FLAKE='@flake@'
 HOST='@host@'
+MAIN='@main@'
 
 ### HELPER FUNCTIONS ###
 
@@ -11,26 +12,20 @@ print_usage() {
 	# Print script usage
 
 	cat <<EOF
-Usage: $0 --main DEVICE [-k KEYFILE] [OPTIONS]
+Usage: $0 [-k KEYFILE] [OPTIONS]
 Install the system on this machine.
 
-    --main                  path to the device with the main disk
 	-k, --keyfile           path to the age key file
 EOF
 }
 
 ### PARSE ARGUMENTS ###
 
-main=''
 keyfile="${SOPS_AGE_KEY_FILE:-${SOPS_AGE_KEY_DIR:-${XDG_CONFIG_HOME:-${HOME}/.config}/sops/age}/keys.txt}"
 unparsed=''
 
 while [[ -n ${1:-} ]]; do
 	case "$1" in
-	--main)
-		shift
-		main="$1"
-		;;
 	-k | --keyfile)
 		shift
 		keyfile="$1"
@@ -52,29 +47,17 @@ done
 # shellcheck disable=SC2086
 set -- ${unparsed}
 
-if [[ -z ${main} ]]; then
-	printf '%s\n' 'Error: --main is required.' >&2
-	print_usage >&2
-	exit 1
-fi
-
-if [[ ! -e ${main} ]]; then
-	printf '%s\n' "Error: Device ${main} does not exist." >&2
-	print_usage >&2
-	exit 2
-fi
-
 if [[ ! -e ${keyfile} ]]; then
 	printf '%s\n' "Error: Key file ${keyfile} does not exist." >&2
 	print_usage >&2
-	exit 3
+	exit 1
 fi
 
 ### MAIN ###
 
 disko-install \
 	--flake "${FLAKE}#${HOST}" \
-	--disk main "${main}" \
+	--disk main "${MAIN}" \
 	--extra-files "${keyfile}" /var/lib/sops/age/keys.txt \
 	--write-efi-boot-entries \
 	"$@"
