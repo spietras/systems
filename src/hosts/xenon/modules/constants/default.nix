e96e5980ebc05d9f32573ea0324e3042a8aed32b
Sebastian Pietras <me@spietras.dev>
2026-09-14 22:31:44 +0200

    Updated `k3s`

--- src/hosts/xenon/modules/constants/default.nix
+++ src/hosts/xenon/modules/constants/default.nix
@@ -105,7 +105,7 @@
         # Update incrementally, one minor version at a time
         # See: https://kubernetes.io/releases/version-skew-policy
         package = lib.mkOption {
-          default = pkgs.k3s_1_34;
+          default = pkgs.k3s_1_35;
           description = "Versioned k3s package to use";
           type = lib.types.package;
         };
