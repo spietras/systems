dc6e037586b8d3874b92dc1782c1ae624a1ebfda
Sebastian Pietras <me@spietras.dev>
2026-09-21 00:31:25 +0200

    Fixed `plymouth` theme (#98)

--- src/hosts/carbon/modules/style/default.nix
+++ src/hosts/carbon/modules/style/default.nix
@@ -34,9 +34,21 @@ in {
 
       themePackages = [
         # Install adi1090x Plymouth themes
-        (pkgs.adi1090x-plymouth-themes.override {
-          selected_themes = ["angular"];
-        })
+        (
+          (
+            pkgs.adi1090x-plymouth-themes.override {
+              selected_themes = ["angular"];
+            }
+          ).overrideAttrs
+          (old: {
+            patches =
+              (old.patches or [])
+              ++ [
+                # Patch to center the animation on multiple monitors
+                ./angular-center.patch
+              ];
+          })
+        )
       ];
     };
   };
